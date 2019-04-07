51a423e9edc57f63f7d61864dc1876123b1bae41
Илья <ilya-31071992@mail.ru>
2019-04-07 17:38:32 +0300

    Задание 25. Добавил генерации случайных данных в тесты

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -1,14 +1,18 @@
+from model.project import Project
 import string
 import random
-from model.project import Project
+import pytest
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
+Testdata = [Project(name=random_username("project", 10))]
 
-
-def test_add_project(app):
+@pytest.mark.parametrize("project", Testdata, ids=[repr(x) for x in Testdata])
+def test_add_project(app, project):
      if len(app.project.get_project_list()) == 0:
-         app.project.create_new_project(Project(name="NewProject6"))
+         app.project.create_new_project(project)
      old_list_project = app.project.get_project_list()
-     project = Project(name="NewProject2")
      app.project.create_new_project(project)
      new_list_project = app.project.get_project_list()
      assert len (old_list_project) + 1 == len (new_list_project)
--- tests/test_delete_project.py
+++ tests/test_delete_project.py
@@ -1,9 +1,19 @@
 from model.project import Project
+import string
 import random
+import pytest
 
-def test_delete_some_project(app):
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+Testdata = [Project(name=random_username("project", 10))]
+
+@pytest.mark.parametrize("project", Testdata, ids=[repr(x) for x in Testdata])
+
+def test_delete_some_project(app, project):
     if len(app.project.get_project_list()) == 0:
-        app.project.create_new_project(Project(name="test4"))
+        app.project.create_new_project(project)
     old_list_project = app.project.get_project_list()
     project = random.choice(old_list_project)
     app.project.delete_project(project)
